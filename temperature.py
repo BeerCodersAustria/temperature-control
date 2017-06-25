dfa72ae3856e3d9e0ea95a9da768fd9d9c2214f4
Alex Hollaus <office@knofy.net>
2017-06-25 21:31:57 +0200

    created wait-function

--- temperature.py
+++ temperature.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
+import time
 
 loop_nr = 0
 while True:
@@ -8,3 +9,9 @@ while True:
 	loop_nr += 1
 	print 'Temp: {0:0.1f} C Humidity: {1:0.1f} % Loop: {2}'.format(temperature, humidity, loop_nr)
 
+def sequence():
+    # main-function of the program
+    # makes sure that the write-function is only executed once an hour
+    while True:
+        write_function()    # insert real-function name
+        time.sleep(3600)    # 1 hour
